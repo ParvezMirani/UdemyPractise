bd6836e86a8cf3495239f11a3c6d87fc1ddce5ce
Parvez Mirani <prvz24@gmail.com>
2021-01-25 20:20:47 +1300

    initial commit done

---
+++ API/Controllers/DepartmentController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("Get All students");
+        }
+
+        [HttpGet("{code}")]
+        public IActionResult GetA(string code)
+        {
+            return Ok("get this " + code + "department data");
+        }
+
+        [HttpPost]
+        public IActionResult Insert()
+        {
+            return Ok("inserted");
+        }
+
+        [HttpPut("{code}")]
+        public IActionResult UpdateADepartment(string code)
+        {
+            return Ok($"updated this {code} is updated");
+        }
+
+        [HttpDelete("{code}")]
+        public IActionResult DeleteADepartment(string code)
+        {
+            return Ok($"deleted {code}");
+        }
+    }
+}
